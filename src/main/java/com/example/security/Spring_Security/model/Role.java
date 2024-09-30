ac35265862638e5480cec8233a1aeb912d292f00
Shuaib <shuaib6950@gmail.com>
2024-09-30 23:00:03 +0600

    Custom User class along with necessary classes has been added

---
+++ src/main/java/com/example/security/Spring_Security/model/Role.java
@@ -0,0 +1,48 @@
+package com.example.security.Spring_Security.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @ToString.Exclude
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, name = "role_name")
+    private AppRole roleName;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY, cascade = {CascadeType.MERGE})
+    @JsonBackReference
+    @ToString.Exclude
+    private Set<User> users = new HashSet<>();
+
+    public Role(AppRole roleName){
+        this.roleName = roleName;
+    }
+}
