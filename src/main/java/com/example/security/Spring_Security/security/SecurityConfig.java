dc6250adcd08724542fe9dfb03a5bec2d57b001c
Shuaib <shuaib6950@gmail.com>
2024-10-18 21:25:32 +0600

    CSRF Configured

--- src/main/java/com/example/security/Spring_Security/security/SecurityConfig.java
+++ src/main/java/com/example/security/Spring_Security/security/SecurityConfig.java
@@ -1,11 +1,10 @@
 package com.example.security.Spring_Security.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import java.time.LocalDate;
-
-import javax.sql.DataSource;
-
+import com.example.security.Spring_Security.model.AppRole;
+import com.example.security.Spring_Security.model.Role;
+import com.example.security.Spring_Security.model.User;
+import com.example.security.Spring_Security.repository.RoleRepository;
+import com.example.security.Spring_Security.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,69 +12,52 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
-import com.example.security.Spring_Security.model.AppRole;
-import com.example.security.Spring_Security.model.Role;
-import com.example.security.Spring_Security.model.User;
-import com.example.security.Spring_Security.repository.RoleRepository;
-import com.example.security.Spring_Security.repository.UserRepository;
+import java.time.LocalDate;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@EnableMethodSecurity(prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
 public class SecurityConfig {
-    // @Bean
-    // SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    //     http.authorizeHttpRequests((requests) -> requests
-    //     .requestMatchers("/check").permitAll()
-    //     .anyRequest()
-    //     .authenticated());
-    //     // http.formLogin(withDefaults());
-    //     http.csrf(AbstractHttpConfigurer::disable);
-    //     http.sessionManagement(session -> 
-    //             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-    //     http.httpBasic(withDefaults());
-    //     return http.build();
-    // }
-
     @Bean
-    SecurityFilterChain defauSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((requests)-> requests.anyRequest().authenticated());
-        http.csrf(AbstractHttpConfigurer::disable);
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf ->
+                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers("/api/auth/public/**")
+        );
+        http.authorizeHttpRequests((requests)
+                -> requests
+                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .requestMatchers("/api/csrf-token").permitAll()
+                .anyRequest().authenticated());
+//        http.csrf(AbstractHttpConfigurer::disable);
+//        http.addFilterBefore(new CustomLoggingFilter(),
+//                UsernamePasswordAuthenticationFilter.class);
+//        http.addFilterAfter(new RequestValidationFilter(), CustomLoggingFilter.class);
+        //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
-        return http.build(); 
+        return http.build();
     }
 
-    // @Bean
-    // UserDetailsService userDetailsService(DataSource dataSource){
-    //     JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
-    //     if(!manager.userExists("user1")){
-    //         manager.createUser(
-    //             org.springframework.security.core.userdetails.User.withUsername("user1")
-    //                 .password("{noop}password1")
-    //                 .roles("USER")
-    //                 .build()
-    //         );
-    //     }
-    //     if(!manager.userExists("admin")){
-    //         manager.createUser(
-    //             org.springframework.security.core.userdetails.User.withUsername("admin")
-    //                 .password("{noop}adminPass")
-    //                 .roles("ADMIN")
-    //                 .build()
-    //         );
-    //     }
-
-    //     return manager;
-    // }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
-    public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository) {
+    public CommandLineRunner initData(RoleRepository roleRepository,
+                                      UserRepository userRepository,
+                                      PasswordEncoder passwordEncoder) {
         return args -> {
             Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
                     .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_USER)));
@@ -84,7 +66,8 @@ public class SecurityConfig {
                     .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN)));
 
             if (!userRepository.existsByUserName("user1")) {
-                User user1 = new User("user1", "user1@example.com", "{noop}password1");
+                User user1 = new User("user1", "user1@example.com",
+                        passwordEncoder.encode("password1"));
                 user1.setAccountNonLocked(false);
                 user1.setAccountNonExpired(true);
                 user1.setCredentialsNonExpired(true);
@@ -96,9 +79,10 @@ public class SecurityConfig {
                 user1.setRole(userRole);
                 userRepository.save(user1);
             }
-            
+
             if (!userRepository.existsByUserName("admin")) {
-                User admin = new User("admin", "admin@example.com", "{noop}adminPass");
+                User admin = new User("admin", "admin@example.com",
+                        passwordEncoder.encode("adminPass"));
                 admin.setAccountNonLocked(true);
                 admin.setAccountNonExpired(true);
                 admin.setCredentialsNonExpired(true);
@@ -112,4 +96,4 @@ public class SecurityConfig {
             }
         };
     }
-} 
+}
